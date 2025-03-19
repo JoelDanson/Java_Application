71658634834b190197b181bb58acf7c536780c52
JoelDanson <joeldanson259@gmail.com>
2025-03-19 12:37:49 +0100

    Print method

---
+++ PrintMethod.java
@@ -0,0 +1,20 @@
+public class PrintMethod{
+     public static void main(String[] args)  {
+	   //using the print
+	   System.out.print("\"Learning Java Is Fun\"\n");
+	   System.out.print (" Hey, Come Learn Java With Me");
+	   
+	   //using the println()
+	   System.out.println();
+	   System.out.println("I Want To Tell You That Java Is Different From Javascript");
+	   System.out.println("\t this is the indented text");
+	   
+	   //using the printf()
+	   System.out.printf("My name is Mercy and i love %s. Good day everyone%n","Java");
+	   System.out.printf("I bought Java JDK with %f naira%n",159.345);
+	   System.out.printf("I bought Java JDK with %c %.2f %n",'$',159.345);
+	   
+	   boolean isJavaFun = true;
+	   System.out.printf("Is java fun? %b",isJavaFun);
+	 }
+}
